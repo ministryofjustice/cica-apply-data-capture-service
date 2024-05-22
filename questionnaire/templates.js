f0cd47e7478cbf546c036d6f44668ffab6e66bbe
tjbburton <Thomas.Burton@CICA.gov.uk>
2024-05-22 09:56:29 +0100

    wip

--- questionnaire/templates.js
+++ questionnaire/templates.js
@@ -2,15 +2,27 @@
 
 const applicationTemplate = require('q-templates-application');
 
+const tcqeTemplate = require('q-templates-tcqe');
+
 const applicationTemplateAsJson = JSON.stringify(applicationTemplate);
+
+const tcqeTemplateAsJson = JSON.stringify(tcqeTemplate);
 
 function getApplicationTemplateCopy() {
     return JSON.parse(applicationTemplateAsJson);
+}
+
+function getTcqeTemplateCopy() {
+    return JSON.parse(tcqeTemplateAsJson);
 }
 
 module.exports = {
     'sexual-assault': id => ({
         id,
         ...getApplicationTemplateCopy()
+    }),
+    tcqe: id => ({
+        id,
+        ...getTcqeTemplateCopy()
     })
 };
