20bef1a8b4ec782edd30e713ab392988afdeed6d
tjbburton <Thomas.Burton@CICA.gov.uk>
2024-05-22 09:56:29 +0100

    wip

--- questionnaire/questionnaire-service.js
+++ questionnaire/questionnaire-service.js
@@ -446,7 +446,7 @@ function createQuestionnaireService({
         // 1 - get questionnaire instance
         const questionnaire = await getQuestionnaire(questionnaireId);
         // 1.1 check questionnaire is a compatible version
-        const isCompatible = isQuestionnaireCompatible(questionnaire.version);
+        const isCompatible = true; //isQuestionnaireCompatible(questionnaire.version);
         // 1.2 if not, return 'incompatible questionnaire' schema
         if (!isCompatible) {
             return {
