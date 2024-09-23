d7966d0e18ecfd2058de9db7061a59590c002003
tjbburton <Thomas.Burton@CICA.gov.uk>
2024-09-23 11:58:02 +0100

    Add support for decision letter

--- questionnaire/templates.js
+++ questionnaire/templates.js
@@ -8,12 +8,18 @@ const applicationTemplateAsJson = JSON.stringify(applicationTemplate);
 
 const tcqeTemplateAsJson = JSON.stringify(letterTemplates.tcqe);
 
+const decisionLetterTemplateAsJson = JSON.stringify(letterTemplates.decisionletter);
+
 function getApplicationTemplateCopy() {
     return JSON.parse(applicationTemplateAsJson);
 }
 
 function getTcqeTemplateCopy() {
     return JSON.parse(tcqeTemplateAsJson);
+}
+
+function getDecisionLetterTemplateCopy() {
+    return JSON.parse(decisionLetterTemplateAsJson);
 }
 
 module.exports = {
@@ -24,5 +30,9 @@ module.exports = {
     tcqe: id => ({
         id,
         ...getTcqeTemplateCopy()
+    }),
+    decisionletter: id => ({
+        id,
+        ...getDecisionLetterTemplateCopy()
     })
 };
