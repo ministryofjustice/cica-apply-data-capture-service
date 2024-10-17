6f348dd5ba9f3a411fc251b35a132e08d9d25419
tjbburton <Thomas.Burton@CICA.gov.uk>
2024-10-17 14:51:20 +0100

    TEMP: remove q-templates-actions while build issues persist

--- questionnaire/templates.js
+++ questionnaire/templates.js
@@ -2,37 +2,15 @@
 
 const applicationTemplate = require('q-templates-application');
 
-const letterTemplates = require('q-templates-actions');
-
 const applicationTemplateAsJson = JSON.stringify(applicationTemplate);
-
-const tcqeTemplateAsJson = JSON.stringify(letterTemplates.tcqe);
-
-const decisionLetterTemplateAsJson = JSON.stringify(letterTemplates.decisionletter);
 
 function getApplicationTemplateCopy() {
     return JSON.parse(applicationTemplateAsJson);
-}
-
-function getTcqeTemplateCopy() {
-    return JSON.parse(tcqeTemplateAsJson);
-}
-
-function getDecisionLetterTemplateCopy() {
-    return JSON.parse(decisionLetterTemplateAsJson);
 }
 
 module.exports = {
     'sexual-assault': id => ({
         id,
         ...getApplicationTemplateCopy()
-    }),
-    tcqe: id => ({
-        id,
-        ...getTcqeTemplateCopy()
-    }),
-    'decision-letter': id => ({
-        id,
-        ...getDecisionLetterTemplateCopy()
     })
 };
