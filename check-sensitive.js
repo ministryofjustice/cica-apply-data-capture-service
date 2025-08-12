77a9819badafdcef6b8bbb90bbe79c3ab22139c5
Adrian Roworth <adrian.roworth@justice.gov.uk>
2025-08-12 11:25:46 +0100

    WIP:

--- check-sensitive.js
+++ check-sensitive.js
@@ -1,5 +1,11 @@
-'use strict';
+// 'use strict';
 
-const fileList = process.argv[2];
+// const fileList = process.argv[2];
 
-console.log(fileList);
+// console.log(fileList);
+
+// throw Error(`${fileList} is not good!`);
+
+// https://github.com/gitleaks/gitleaks
+// https://github.com/secretlint/secretlint
+// https://github.com/lint-staged/lint-staged#configuration
