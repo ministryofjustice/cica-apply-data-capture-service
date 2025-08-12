627d3c3264a9187df23b0fb2c36e47e6d9d64fc0
Adrian Roworth <adrian.roworth@justice.gov.uk>
2025-08-12 10:53:49 +0100

    WIP:

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,8 @@ const config = {
     '*.js': ['eslint --fix --color'],
     '*.{json,yml,yaml}': ['prettier --write'],
     './openapi/openapi.json': 'npm run lint:openapi',
-    './openapi/openapi-admin.json': 'npm run lint:openapi-admin'
+    './openapi/openapi-admin.json': 'npm run lint:openapi-admin',
+    '*': 'node ./check-sensitive.js'
 };
 
 module.exports = config;
---
+++ check-sensitive.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const fileList = process.argv[2];
+
+console.log(fileList);
