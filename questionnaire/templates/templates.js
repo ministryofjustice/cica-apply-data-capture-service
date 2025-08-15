d61dc55fddce5e1321eccc610835ae479779448c
tjbburton <Thomas.Burton@CICA.gov.uk>
2025-08-15 10:00:29 +0100

    Bump q-templates-application to v12.4.0

--- questionnaire/templates/templates.js
+++ questionnaire/templates/templates.js
@@ -10,7 +10,8 @@ const supportedTemplates = {
         '12.3.9': {module: 'q-templates-application-12_3_9'},
         '12.3.10': {module: 'q-templates-application-12_3_10'},
         '12.3.11': {module: 'q-templates-application-12_3_11'},
-        '12.3.12': {module: 'q-templates-application', latest: true}
+        '12.3.12': {module: 'q-templates-application-12_3_12'},
+        '12.4.0': {module: 'q-templates-application', latest: true}
     }
 };
 
