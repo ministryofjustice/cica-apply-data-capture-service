9e3193c8a5a78f69c0c1484877ff6a4bcc270b6d
tjbburton <Thomas.Burton@CICA.gov.uk>
2025-10-20 16:23:08 +0100

    Add support for new review template

--- questionnaire/templates/templates.js
+++ questionnaire/templates/templates.js
@@ -15,6 +15,9 @@ const supportedTemplates = {
         '12.4.1': {module: 'q-templates-application-12_4_1'},
         '12.4.2': {module: 'q-templates-application-12_4_2'},
         '12.4.3': {module: 'q-templates-application', latest: true}
+    },
+    'request-a-review': {
+        '1.0.0': {module: 'q-templates-review', latest: true}
     }
 };
 
